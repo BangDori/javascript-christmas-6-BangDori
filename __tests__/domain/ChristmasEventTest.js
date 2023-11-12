28140f5e2ebdf1780c4513533ab5cf5161bc21a9
BangDori <qudwns8865@naver.com>
2023-11-12 20:49:30 +0900

    test: 크리스마스 이벤트 테스트 추가

---
+++ __tests__/domain/ChristmasEventTest.js
@@ -0,0 +1,21 @@
+import ChristmasEvent from '../../src/domain/ChristmasEvent';
+
+describe('크리스마스 이벤트 클래스 테스트', () => {
+  test('1일, 1000원 할인', () => {
+    const discount = ChristmasEvent.getDiscount(1);
+
+    expect(discount).toBe(1000);
+  });
+
+  test('25일, 3400원 할인', () => {
+    const discount = ChristmasEvent.getDiscount(25);
+
+    expect(discount).toBe(3400);
+  });
+
+  test('26일, 할인 없음', () => {
+    const discount = ChristmasEvent.getDiscount(26);
+
+    expect(discount).toBe(0);
+  });
+});
