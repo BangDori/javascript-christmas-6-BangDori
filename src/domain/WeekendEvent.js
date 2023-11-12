0d8f076f2abfe2ef9caaac040c240ee202a48a05
BangDori <qudwns8865@naver.com>
2023-11-12 22:07:27 +0900

    feat: 주말 이벤트 클래스 추가

---
+++ src/domain/WeekendEvent.js
@@ -0,0 +1,26 @@
+import { WEEKEND } from '../constant/date.js';
+import { WEEKEND_EVENT } from '../constant/event.js';
+import { getCurrentDayType } from '../util/date.js';
+
+class WeekendEvent {
+  static getDiscount(date, mainCount) {
+    const day = getCurrentDayType(date);
+    if (day !== WEEKEND.name) {
+      return 0;
+    }
+
+    const discount = mainCount * WEEKEND_EVENT.perMain;
+    return discount;
+  }
+
+  static getDetailInfo(mainCount) {
+    const discount = this.getDiscount(mainCount);
+    if (discount === 0) {
+      return null;
+    }
+
+    return `${WEEKEND_EVENT.name}: -${discount.toLocaleString()}원`;
+  }
+}
+
+export default WeekendEvent;
