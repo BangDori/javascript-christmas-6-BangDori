979b7d04c67e615b0872ef835df1107bcf8d4a9e
BangDori <qudwns8865@naver.com>
2023-11-12 23:49:48 +0900

    test: 주문 상세 정보에 대한 테스트 추가

--- __tests__/domain/OrderInfoTest.js
+++ __tests__/domain/OrderInfoTest.js
@@ -1,16 +1,26 @@
 import OrderInfo from '../../src/domain/OrderInfo';
 
 describe('주문 정보 클래스 테스트', () => {
-  test('총 주문 금액 테스트', () => {
-    const menuList = [
-      ['양송이수프', 1],
-      ['티본스테이크', 1],
-      ['초코케이크', 1],
-      ['레드와인', 1],
-    ];
+  const menuList = [
+    ['양송이수프', 1],
+    ['티본스테이크', 1],
+    ['초코케이크', 1],
+    ['레드와인', 1],
+  ];
+  const orderInfo = new OrderInfo(menuList);
 
-    const orderInfo = new OrderInfo(menuList);
+  test('총 주문 금액 테스트', () => {
     const orderAmount = orderInfo.getOrderAmount();
     expect(orderAmount).toBe(136000);
+  });
+
+  test('주문 상세 정보 테스트', () => {
+    const recepitDetails = orderInfo.getReceiptDetails();
+
+    recepitDetails.forEach((recepitDetail, idx) => {
+      const [name, count] = menuList[idx];
+
+      expect(recepitDetail).toBe(`${name} ${count}개`);
+    });
   });
 });
