e4b6bb1b63ea33543e551e79aebba7b51cb45c99
BangDori <qudwns8865@naver.com>
2023-11-12 21:19:05 +0900

    test: 특별 할인 이벤트 테스트 추가

---
+++ __tests__/domain/SpecialEventTest.js
@@ -0,0 +1,21 @@
+import SpecialEvent from '../../src/domain/SpecialEvent';
+
+describe('특별 할인 이벤트 테스트', () => {
+  test('일요일 특별 할인 적용', () => {
+    const date = 3; // 12.03 일요일
+    const discount = SpecialEvent.getDiscount(date);
+
+    expect(discount).toBe(1000);
+  });
+
+  test('일요일 외 다른 요일 특별 할인 미적용', () => {
+    // 12.04 월요일 ~ 12.09 금요일
+    const dates = [4, 5, 6, 7, 8, 9];
+
+    dates.forEach(date => {
+      const discount = SpecialEvent.getDiscount(date);
+
+      expect(discount).toBe(0);
+    });
+  });
+});
