e80d44fe5f2c217d57771e86bdb07d90190fe279
BangDori <qudwns8865@naver.com>
2023-11-12 20:34:47 +0900

    test: 배지 이벤트 테스트 추가

---
+++ __tests__/domain/BadgeEventTest.js
@@ -0,0 +1,19 @@
+import BadgeEvent from '../../src/domain/BadgeEvent';
+
+describe('배지 이벤트 클래스 테스트', () => {
+  test.each([
+    { price: 20_000, expectedBadge: '산타' },
+    { price: 10_000, expectedBadge: '트리' },
+    { price: 5_000, expectedBadge: '별' },
+  ])('$expectedBadge 배지 테스트', ({ price, expectedBadge }) => {
+    const badge = BadgeEvent.getBadge(price);
+
+    expect(badge).toBe(expectedBadge);
+  });
+
+  test('5000원 이하인 경우 배지 없음', () => {
+    const badge = BadgeEvent.getBadge(4999);
+
+    expect(badge).toBe('없음');
+  });
+});
