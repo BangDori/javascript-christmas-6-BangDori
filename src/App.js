81a653864a876270b5b1884c41bd2cb0bdda3a40
BangDori <qudwns8865@naver.com>
2023-11-12 18:55:07 +0900

    refactor: 사용자 입력부 분리

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import OutputView from './view/OutputView.js';
 
 class App {
   async run() {
+    const { date, menuList } = await this.inputOrderInfo();
+  }
+
+  async inputOrderInfo() {
     OutputView.printWelcome();
 
     const date = await retryOnError(InputView.readDate);
     const menuList = await retryOnError(InputView.readMenuList);
 
     OutputView.printPreviewEvent(date);
+    return { date, menuList };
   }
 }
 
