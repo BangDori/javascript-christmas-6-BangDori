7243ab966372644cff26642fbcae1c1003fd2b73
BangDori <qudwns8865@naver.com>
2023-11-12 21:18:33 +0900

    feat: 숫자와 요일을 가지는 객체 추가

--- src/constant/date.js
+++ src/constant/date.js
@@ -1,6 +1,17 @@
 export const EVENT_DATE = Object.freeze({
+  year: 2023,
   month: 12,
   start: 1,
   end: 31,
   christmas: 25,
+});
+
+export const DAY = Object.freeze({
+  0: '일요일',
+  1: '월요일',
+  2: '화요일',
+  3: '수요일',
+  4: '목요일',
+  5: '금요일',
+  6: '토요일',
 });
