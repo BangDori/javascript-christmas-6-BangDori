9fcc51acf9696f0b9cd4963f7eb8f330827bf207
BangDori <qudwns8865@naver.com>
2023-11-13 23:41:16 +0900

    test: 중복되는 메뉴 테스트 및 정상 검증 테스트 추가

--- __tests__/validator/MenuValidatorTest.js
+++ __tests__/validator/MenuValidatorTest.js
@@ -2,19 +2,28 @@ import { MENU_ERROR } from '../../src/constant/message/error';
 import MenuValidator from '../../src/validator/MenuValidator';
 
 describe('메뉴 검증 클래스 테스트', () => {
-  describe('메뉴 존재 여부 테스트', () => {
-    test('존재하는 메뉴에 대한 테스트', () => {
-      const menuList = ['샴페인', '티본스테이크'];
+  test('정상 검증 테스트', () => {
+    const menuList = [
+      ['샴페인', 1],
+      ['티본스테이크', 2],
+    ];
 
-      expect(() => MenuValidator.validateMenuExistence(menuList)).not.toThrow();
-    });
+    expect(() => MenuValidator.validate(menuList)).not.toThrow();
+  });
 
-    test('존재하지 않는 메뉴에 대한 테스트', () => {
-      const menuList = ['햄버거', '제로콜라'];
+  test('존재하지 않는 메뉴에 대한 테스트', () => {
+    const menuList = ['햄버거', '제로콜라'];
 
-      expect(() => MenuValidator.validateMenuExistence(menuList)).toThrow(
-        MENU_ERROR,
-      );
-    });
+    expect(() => MenuValidator.validateMenuExistence(menuList)).toThrow(
+      MENU_ERROR,
+    );
+  });
+
+  test('중복되는 메뉴에 대한 테스트', () => {
+    const menuList = ['샴페인', '티본스테이크', '티본스테이크'];
+
+    expect(() => MenuValidator.validateMenuDuplication(menuList)).toThrow(
+      MENU_ERROR,
+    );
   });
 });
