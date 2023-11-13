d622b05ad626cb0537d45600b0cedb1f077a0b80
BangDori <qudwns8865@naver.com>
2023-11-13 23:58:07 +0900

    test: 메뉴 검증 테스트 추가

--- __tests__/validator/InputValidatorTest.js
+++ __tests__/validator/InputValidatorTest.js
@@ -1,4 +1,4 @@
-import { DATE_ERROR } from '../../src/constant/message/error';
+import { DATE_ERROR, MENU_ERROR } from '../../src/constant/message/error';
 import InputValidator from '../../src/validator/InputValidator';
 
 describe('입력 검증 클래스 테스트', () => {
@@ -16,6 +16,24 @@ describe('입력 검증 클래스 테스트', () => {
         console.log(invalidDate);
         expect(() => InputValidator.validateDate(invalidDate)).toThrow(
           DATE_ERROR,
+        );
+      });
+    });
+  });
+
+  describe('메뉴 검증 테스트', () => {
+    test('유효 테스트', () => {
+      const validMenuList = '김치찌개-1,된장찌개-1';
+
+      expect(() => InputValidator.validateMenu(validMenuList)).not.toThrow();
+    });
+
+    test('유효하지 않는 주문에 대한 테스트', () => {
+      const invalidMenuList = ['음료-1, 메뉴', '음료', 'abc-1'];
+
+      invalidMenuList.forEach(invalidMenu => {
+        expect(() => InputValidator.validateMenu(invalidMenu)).toThrow(
+          MENU_ERROR,
         );
       });
     });
