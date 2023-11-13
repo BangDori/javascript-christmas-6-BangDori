3c0ca0bde0d8e6c3f6af2a807c5cbba62d2be6b9
BangDori <qudwns8865@naver.com>
2023-11-13 14:26:32 +0900

    test: 크리스마스 이벤트 클래스 테스트 수정

--- __tests__/domain/ChristmasEventTest.js
+++ __tests__/domain/ChristmasEventTest.js
@@ -1,21 +1,24 @@
+import { CHRISTMAS_EVENT } from '../../src/constant/event';
 import ChristmasEvent from '../../src/domain/event/ChristmasEvent';
 
 describe('크리스마스 이벤트 클래스 테스트', () => {
   test('1일, 1000원 할인', () => {
-    const discount = ChristmasEvent.getDiscount(1);
+    const christmasEvent = ChristmasEvent.getBenefitInfo(1);
 
-    expect(discount).toBe(1000);
+    expect(christmasEvent.event).toBe(CHRISTMAS_EVENT.name);
+    expect(christmasEvent.discount).toBe(1000);
   });
 
   test('25일, 3400원 할인', () => {
-    const discount = ChristmasEvent.getDiscount(25);
+    const christmasEvent = ChristmasEvent.getBenefitInfo(25);
 
-    expect(discount).toBe(3400);
+    expect(christmasEvent.event).toBe(CHRISTMAS_EVENT.name);
+    expect(christmasEvent.discount).toBe(3400);
   });
 
   test('26일, 할인 없음', () => {
-    const discount = ChristmasEvent.getDiscount(26);
+    const christmasEvent = ChristmasEvent.getBenefitInfo(26);
 
-    expect(discount).toBe(0);
+    expect(christmasEvent).toBe(null);
   });
 });
