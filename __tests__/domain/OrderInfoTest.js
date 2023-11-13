ffa48dd20db5b854b212604fcc1c17d923267251
BangDori <qudwns8865@naver.com>
2023-11-13 14:44:09 +0900

    refactor: 특별 할인 및 주문 정보 클래스 테스트 수정

--- __tests__/domain/OrderInfoTest.js
+++ __tests__/domain/OrderInfoTest.js
@@ -1,13 +1,14 @@
 import OrderInfo from '../../src/domain/OrderInfo';
 
 describe('주문 정보 클래스 테스트', () => {
+  const date = 1;
   const menuList = [
     ['양송이수프', 1],
     ['티본스테이크', 1],
     ['초코케이크', 1],
     ['레드와인', 1],
   ];
-  const orderInfo = new OrderInfo(menuList);
+  const orderInfo = new OrderInfo(date, menuList);
 
   test('총 주문 금액 테스트', () => {
     const orderAmount = orderInfo.getOrderAmount();
--- __tests__/domain/SpecialEventTest.js
+++ __tests__/domain/SpecialEventTest.js
@@ -3,7 +3,7 @@ import SpecialEvent from '../../src/domain/event/SpecialEvent';
 describe('특별 할인 이벤트 테스트', () => {
   test('일요일 특별 할인 적용', () => {
     const date = 3; // 12.03 일요일
-    const discount = SpecialEvent.getDiscount(date);
+    const { discount } = SpecialEvent.getBenefitInfo(date);
 
     expect(discount).toBe(1000);
   });
@@ -13,9 +13,9 @@ describe('특별 할인 이벤트 테스트', () => {
     const dates = [4, 5, 6, 7, 8, 9];
 
     dates.forEach(date => {
-      const discount = SpecialEvent.getDiscount(date);
+      const event = SpecialEvent.getBenefitInfo(date);
 
-      expect(discount).toBe(0);
+      expect(event).toBe(null);
     });
   });
 });
