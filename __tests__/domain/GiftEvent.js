421ac1d7c86f4efcb912811282c50078fd62516e
BangDori <qudwns8865@naver.com>
2023-11-13 14:29:16 +0900

    test: 증정 이벤트 테스트 수정

--- __tests__/domain/GiftEvent.js
+++ __tests__/domain/GiftEvent.js
@@ -1,16 +1,17 @@
-import MENU_BOARD from '../../src/constant/menu/board';
+import { GIFT_EVENT } from '../../src/constant/event';
 import GiftEvent from '../../src/domain/event/GiftEvent';
 
 describe('증정 이벤트 테스트', () => {
   test('12만원 이상일 경우 샴페인 증정', () => {
-    const gift = GiftEvent.getGift(120_000);
+    const gift = GiftEvent.getBenefitInfo(120_000);
 
-    expect(gift).toBe(MENU_BOARD['샴페인']);
+    expect(gift.event).toBe(GIFT_EVENT.name);
+    expect(gift.name).toBe('샴페인');
   });
 
   test('12만원보다 적을 경우 샴페인 미증정', () => {
-    const gift = GiftEvent.getGift(120_000 - 1);
+    const gift = GiftEvent.getBenefitInfo(120_000 - 1);
 
-    expect(gift).toBe('없음');
+    expect(gift).toBe(null);
   });
 });
