ede97cf4c7c7face93db4da1ac78cf312c43369f
BangDori <qudwns8865@naver.com>
2023-11-14 20:41:43 +0900

    style: 혜택 금액 변수명 수정

--- src/domain/event/BadgeEvent.js
+++ src/domain/event/BadgeEvent.js
@@ -1,16 +1,16 @@
 import { EVENT_BADGE, NO_BENEFIT } from '../../constant/event.js';
 
 class BadgeEvent {
-  static getBadge(totalEventPrice) {
-    if (totalEventPrice >= EVENT_BADGE.santa.required) {
+  static getBadge(benefitAmount) {
+    if (benefitAmount >= EVENT_BADGE.santa.required) {
       return EVENT_BADGE.santa.name;
     }
 
-    if (totalEventPrice >= EVENT_BADGE.tree.required) {
+    if (benefitAmount >= EVENT_BADGE.tree.required) {
       return EVENT_BADGE.tree.name;
     }
 
-    if (totalEventPrice >= EVENT_BADGE.star.required) {
+    if (benefitAmount >= EVENT_BADGE.star.required) {
       return EVENT_BADGE.star.name;
     }
 
