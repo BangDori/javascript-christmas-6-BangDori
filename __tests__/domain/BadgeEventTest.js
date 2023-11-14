6d6ef175d375d52bf96cc6395f1a25ec4fc47576
BangDori <qudwns8865@naver.com>
2023-11-14 14:06:23 +0900

    style: 테스트 명칭 수정 (5000원 보다 적은 경우 배지 미지급)

--- __tests__/domain/BadgeEventTest.js
+++ __tests__/domain/BadgeEventTest.js
@@ -11,7 +11,7 @@ describe('배지 이벤트 클래스 테스트', () => {
     expect(badge).toBe(expectedBadge);
   });
 
-  test('5000원 이하인 경우 배지 없음', () => {
+  test('5000원 보다 적은 경우 배지 미지급', () => {
     const badge = BadgeEvent.getBadge(4999);
 
     expect(badge).toBe('없음');
